43ee4fc918d5354d6927685f5ea2db251e596ef8
kamalakars04 <kamalakars04@gmail.com>
2020-09-27 00:44:26 +0530

    [Rao].Added to check Equality UC2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Line_Comparision
 {
@@ -7,17 +8,27 @@ namespace Line_Comparision
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            
-            Console.WriteLine("Enter the co-ordinates of one end in x,y format");
-            string[] first_Point = (Console.ReadLine()).Split(",");
-            double x1 = Convert.ToDouble(first_Point[0]);
-            double y1 = Convert.ToDouble(first_Point[1]);
-            Console.WriteLine("Enter the co-ordinates of other end in x,y format");
-            string[] second_Point = (Console.ReadLine()).Split(",");
-            double x2 = Convert.ToDouble(second_Point[0]);
-            double y2 = Convert.ToDouble(second_Point[1]);
-            double length = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-            Console.WriteLine("Length of given line is {0}",length);
+
+            ArrayList length = new ArrayList();
+            for (int i = 1; i < 3;i++)
+                {
+                    Console.WriteLine("Enter the co-ordinates of one end of line {0} in x,y format", i);
+                    string[] first_Point = (Console.ReadLine()).Split(",");
+                    double x1 = Convert.ToDouble(first_Point[0]);
+                    double y1 = Convert.ToDouble(first_Point[1]);
+                    Console.WriteLine("Enter the co-ordinates of other end of line {0} in x,y format",i);
+                    string[] second_Point = (Console.ReadLine()).Split(",");
+                    double x2 = Convert.ToDouble(second_Point[0]);
+                    double y2 = Convert.ToDouble(second_Point[1]);
+                    double len = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+                    length.Add(len);
+                    Console.WriteLine("Length of given line{0} is {1}",i, len);
+                }
+            if (length[0] == length[1])
+                Console.WriteLine("The lengths of both the lines are equal");
+            else
+                Console.WriteLine("The lengths of both the lines are not equal");
+
 
 
         }
