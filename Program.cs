b1400595f64f0af2c18b5ce8e5ab24ea3c5f7230
kamalakars04 <70007234+kamalakars04@users.noreply.github.com>
2020-09-28 08:54:07 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,13 +14,13 @@ namespace Line_Comparision
             for (int i = 1; i < 3;i++)
                 {
                     Console.WriteLine("Enter the co-ordinates of one end of line {0} in x,y format", i);
-                    string[] first_Point = (Console.ReadLine()).Split(",");
-                    double x1 = Convert.ToDouble(first_Point[0]);
-                    double y1 = Convert.ToDouble(first_Point[1]);
+                    string[] firstPoint = (Console.ReadLine()).Split(",");
+                    double x1 = Convert.ToDouble(firstPoint[0]);
+                    double y1 = Convert.ToDouble(firstPoint[1]);
                     Console.WriteLine("Enter the co-ordinates of other end of line {0} in x,y format",i);
-                    string[] second_Point = (Console.ReadLine()).Split(",");
-                    double x2 = Convert.ToDouble(second_Point[0]);
-                    double y2 = Convert.ToDouble(second_Point[1]);
+                    string[] secondPoint = (Console.ReadLine()).Split(",");
+                    double x2 = Convert.ToDouble(secondPoint[0]);
+                    double y2 = Convert.ToDouble(secondPoint[1]);
                     double len = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
                     length.Add(len);
                     Console.WriteLine("Length of given line{0} is {1}",i, len);
